4de8b51d393dd842ddb7f6ad77e433542c2ae8cc
gialngramadhan <gilangsheva2005@gmail.com>
2025-04-27 15:11:55 +0700

    Initial commit

---
+++ app/about.tsx
@@ -0,0 +1,13 @@
+﻿import { View, Text, Button } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function AboutScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>ℹ Ini About Screen</Text>
+      <Link href="/" asChild>
+        <Button title="Kembali ke Home" />
+      </Link>
+    </View>
+  );
+}
---
+++ app/contact.tsx
@@ -0,0 +1,13 @@
+﻿import { View, Text, Button } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function ContactScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> Ini Contact Screen</Text>
+      <Link href="/" asChild>
+        <Button title="Kembali ke Home" />
+      </Link>
+    </View>
+  );
+}
